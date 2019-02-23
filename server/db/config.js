d014826be34f2352d41ac5cb7814b9171ddda686
Mark Frozen <apexof@gmail.com>
2019-02-23 15:09:46 +0300

    добавил аутентичную монгоДБ

--- server/db/config.js
+++ server/db/config.js
@@ -4,6 +4,7 @@ const PORT = NODE_ENV === "development" ? 3000 : process.env.PORT;
 const DEV_URI = "mongodb://localhost:27017/costAccounting";
 const PROD_URI = process.env.MONGODB_URI;
 
-const MONGODB_URI = NODE_ENV === "development" ? DEV_URI : PROD_URI;
+// const MONGODB_URI = NODE_ENV === "development" ? DEV_URI : PROD_URI;
+const MONGODB_URI = "mongodb+srv://apexof:Florida_6@take-my-money-qnhtm.mongodb.net/test?retryWrites=true";
 
 module.exports = { PORT, MONGODB_URI };
