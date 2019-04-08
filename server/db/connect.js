2cecae5cf1078713ba8230de79ceef02db69010a
Mark Frozen <apexof@gmail.com>
2019-04-08 18:53:36 +0300

    pass

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose.connection.once("open", () => console.log("Connected to MongoDB"));
-
+const pass = "Нет пароля";
 function connect(userName) {
   const dbName = userName.replace(/(@|\.)/g, "-");
-  const MONGODB_URI = `mongodb+srv://apexof:Florida_6@take-my-money-qnhtm.mongodb.net/${dbName}?retryWrites=true`;
+  const MONGODB_URI = `mongodb+srv://apexof:${pass}@take-my-money-qnhtm.mongodb.net/${dbName}?retryWrites=true`;
   return mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 }
 
